0537e36e6aee807a6634d78dff6f7aef5d325594
corkyrapier <worldegora@gmail.com>
2021-12-15 04:46:40 +0300

    work with templates

--- testsite/news/views.py
+++ testsite/news/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import News
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'News list'}
+    
+    return render(request, 'news/index.html', context)
 
-def test(request):
-    return HttpResponse('<h1>Test page</h1>')
