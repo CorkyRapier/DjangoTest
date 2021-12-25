42ad85152b76c3fa9db765fe783a9fff0b4c4718
Egor <worldEGORa@gmail.com>
2021-12-25 13:46:14 +0300

    work with templates and tags

--- testsite/news/views.py
+++ testsite/news/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 def index(request):
     news = News.objects.all()
     context = {
         'news': news,
-        'title': 'News list'}
-    
+        'title': 'News list',
+        }
     return render(request, 'news/index.html', context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    
+    return render(request, template_name='news/category.html', context={'news': news, 'category': category})
