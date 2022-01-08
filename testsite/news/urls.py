addca20a49b15b0ea4679977ad5f28df58620f5f
Egor <worldEGORa@gmail.com>
2022-01-08 13:40:14 +0300

    work with classes

--- testsite/news/urls.py
+++ testsite/news/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import get_category, index, view_news, add_news
+from .views import CategoryNews, CreateNews, HomeNews, ViewNews
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('category/<int:category_id>/', get_category, name='category'),
-    path('news/<int:news_id>/', view_news, name='view_news'),
-    path('news/add-news/', add_news, name='add_news'),
+    # path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'),
+    # path('category/<int:category_id>/', get_category, name='category'),
+    path('category/<int:category_id>/', CategoryNews.as_view(), name='category'),
+    # path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
+    # path('news/add-news/', add_news, name='add_news'),
+    path('news/add-news/', CreateNews.as_view(), name='add_news')
+
 ]
